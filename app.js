6be9649182451986661abc782c44a6edbedcad1b
lisanf <lisandrof1994@gmail.com>
2022-12-15 19:31:14 -0300

    Login + Register

--- app.js
+++ app.js
@@ -13,3 +13,18 @@ app.listen(3030, () => {
 app.get('/',(req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
 });
+
+app.get("/home", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get("/login", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
+
+app.get("/register", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+
+
+
